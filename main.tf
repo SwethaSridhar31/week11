1555639df54daeeb593be1341b48f083d8b67ce0
SwethaSridhar31 <159956752+SwethaSridhar31@users.noreply.github.com>
2024-03-27 20:37:09 +0530

    removed egress

--- main.tf
+++ main.tf
@@ -69,12 +69,7 @@ resource "aws_security_group" "web-sg" {
     cidr_blocks = ["10.0.0.0/24"]
   }
   // connectivity to ubuntu mirrors is required to run `apt-get update` and `apt-get install apache2`
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+  
 }
 
 output "web-address" {
